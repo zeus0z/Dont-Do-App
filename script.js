7c9a25384fe318bd672d95e3b510efff8349eaf4
Zeus Amorim <amorimzeus@gmail.com>
2022-08-11 02:48:31 -0300

    Created base layout

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+<div id="task" class=" card">
+                <h5 class="card-header">Task 1</h5>
+                <div class="card-body">
+                    <span class="card-subtitle text-muted"> 11/08/2022 </span>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti necessitatibus quas quae nemo est repellat sapiente! Possimus maxime rem est, nesciunt error provident debitis et expedita quia, aperiam eos officiis!</p>
+                    <div id="actions" class="d-flex justify-content-center">
+                        <button id="edit" class="btn btn-primary mx-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                        <button id="delete" class="btn btn-danger mx-3"><i class="fa-solid fa-trash"></i></button>
+                    </div>
+                </div>
+            </div>
+*/
